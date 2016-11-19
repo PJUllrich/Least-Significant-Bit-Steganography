00c3823b49ecb26d80ede28bb7299e3928dfc303
Peter Ullrich <peter.j.ullrich@web.de>
2016-11-19 21:12:01 +0100

    Wrote an initial parameter reader for the command input. Nothing pretty and not fail-proof, but it works for now.

--- src/com/peterullrich/Main.java
+++ src/com/peterullrich/Main.java
@@ -1,8 +1,43 @@
 package com.peterullrich;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
+    private String secretMessage;
+    private BufferedImage picture;
 
     public static void main(String[] args) {
-	// write your code here
+        String message = null;
+        BufferedImage pictureBitMap = null;
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-m"))
+                message = args[i+1];
+
+            if (args[i].contains(".png")) {
+                try {
+                    File file = new File(args[i]);
+                    pictureBitMap = ImageIO.read(file);
+                } catch (IOException e) {
+                    System.err.println("Could not read image with give file path.");
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if (message != null && pictureBitMap != null) {
+            new Main(message, pictureBitMap);
+        } else {
+            System.err.println("Could not read in the necessary input options.");
+            System.exit(-1);
+        }
+    }
+
+    public Main(String secretMessage, BufferedImage picture) {
+        this.secretMessage = secretMessage;
+        this.picture = picture;
     }
 }
