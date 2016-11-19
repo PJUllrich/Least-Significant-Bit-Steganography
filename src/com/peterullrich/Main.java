397a6b52c89a36fbf0db324e9658657458327ee2
Peter Ullrich <peter.j.ullrich@web.de>
2016-11-19 23:47:42 +0100

    Finished the message adding algorithm to my temporary satisfaction. Watch out when working with Integer.toBinaryString(int i) as it might return a String with less than 8 binary digits in it. Next step: Read a hidden message from a picture.

--- src/com/peterullrich/Main.java
+++ src/com/peterullrich/Main.java
@@ -3,7 +3,9 @@ package com.peterullrich;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
@@ -25,6 +27,24 @@ public class Main {
                     e.printStackTrace();
                 }
             }
+
+            if (args[i].equals("-f")) {
+                try {
+                    FileReader fileReader = new FileReader(args[i+1]);
+
+                    // Always wrap FileReader in BufferedReader.
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                    String line;
+                    message = "";
+                    while((line = bufferedReader.readLine()) != null) {
+                        message += " " + line;
+                    }
+                } catch (IOException e) {
+                    System.err.println("Could not read in given file.");
+                    e.printStackTrace();
+                }
+            }
         }
 
         if (message != null && pictureBitMap != null) {
@@ -35,44 +55,82 @@ public class Main {
         }
     }
 
+    private static String readFile(String path) {
+        File file = new File(path);
+        return file.toString();
+    }
+
     private Main(String secretMessage, BufferedImage picture) {
         this.addMessageToImage(secretMessage, picture);
     }
 
     private void addMessageToImage(String secretMessage, BufferedImage picture) {
         String binaryMessage = this.convertStringToBinary(secretMessage);
+        Boolean endBlockAppended = false;
 
         for (int row = 0; row < picture.getHeight(); row++) {
-            int startIndex = row * picture.getWidth();
-            int endIndex = (row + 1) * picture.getWidth() - 1;
-
-            if (endIndex > binaryMessage.length()) {
-                endIndex = binaryMessage.length() - 1;
-            } else if (startIndex > binaryMessage.length()) {
-                return;
-            }
-
-            String messageLine = binaryMessage.substring(startIndex, endIndex);
-
             for (int column = 0; column < picture.getWidth(); column++) {
-                String messageBlock = messageLine.substring(column * 6, column * 6 + 5);
+
+                int start = row * picture.getWidth() + column * 8;
+                int end = start + 8;
+
+
+
+                String messageBlock;
+                if (end < binaryMessage.length()) {
+                    System.out.println(start + " " + end);
+                    messageBlock = binaryMessage.substring(start, end);
+                } else {
+                    messageBlock = "11111111";
+                    endBlockAppended = true;
+                }
+
                 int rgb = picture.getRGB(column, row);
+                Color oldColor = new Color(rgb, true);
 
-                int red = (rgb >> 16) & 0xFF;
-                int green = (rgb >> 8) & 0xFF;
-                int blue = rgb & 0xFF;
+                String originalRed = fillString(Integer.toBinaryString(oldColor.getRed()));
+                String originalGreen = fillString(Integer.toBinaryString(oldColor.getGreen()));
+                String originalBlue = fillString(Integer.toBinaryString(oldColor.getBlue()));
+                String originalAlpha = fillString(Integer.toBinaryString(oldColor.getAlpha()));
 
-                String sRed = Integer.toBinaryString(red);
-                String sGreen = Integer.toBinaryString(green);
-                String sBlue = Integer.toBinaryString(blue);
+                if (end == 2376) {
+                    System.out.println("break here");
+                }
 
-                String newRed = sRed.substring(0, 5) + messageBlock.substring(0, 1);
-                String newGreen = sGreen.substring(0, 5) + messageBlock.substring(2, 3);
-                String newBlue = sBlue.substring(0, 5) + messageBlock.substring(4, 5);
+                String changedRed = originalRed.substring(0, 6) + messageBlock.substring(0, 2);
+                String changedGreen = originalGreen.substring(0, 6) + messageBlock.substring(2, 4);
+                String changedBlue = originalBlue.substring(0, 6) + messageBlock.substring(4, 6);
+                String changedAlpha = originalAlpha.substring(0, 6) + messageBlock.substring(6);
 
-                Color color = new Color(Integer.parseInt(newRed), Integer.parseInt(newGreen), Integer.parseInt(newBlue));
+                int newRed = Integer.parseInt(changedRed);
+                int newGreen = Integer.parseInt(changedGreen);
+                int newBlue = Integer.parseInt(changedBlue);
+                int newAlpha = Integer.parseInt(changedAlpha);
+
+                newRed = newRed > 255 ? 255 : newRed;
+                newGreen = newGreen > 255 ? 255 : newGreen;
+                newBlue = newBlue > 255 ? 255 : newBlue;
+                newAlpha = newAlpha > 255 ? 255 : newAlpha;
+
+                Color color = new Color(newRed, newGreen, newBlue, newAlpha);
                 picture.setRGB(column, row, color.getRGB());
+
+                if (endBlockAppended)
+                    break;
             }
+        }
+
+        this.saveFile(picture);
+    }
+
+    private void saveFile(BufferedImage picture) {
+        try {
+            File output = new File("output.png");
+            ImageIO.write(picture, "png", output);
+            System.out.println("Saved output in: " + output.getAbsolutePath());
+        } catch (IOException e) {
+            System.err.println("Could not write the output to .png file.");
+            e.printStackTrace();
         }
     }
 
@@ -80,9 +138,18 @@ public class Main {
         String out = "";
 
         char[] charMessage = text.toCharArray();
-        for (char c : charMessage)
-            out += Integer.toBinaryString(c);
+        for (int c = 0; c < charMessage.length; c++){
+            String temp = Integer.toBinaryString(charMessage[c]);
+            out += fillString(temp);
+        }
 
         return out;
+    }
+
+    private String fillString(String temp) {
+        for (int i = temp.length(); i < 8; i++)
+            temp = "0" + temp;
+
+        return temp;
     }
 }
